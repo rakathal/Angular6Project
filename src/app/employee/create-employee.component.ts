cf8f6da08a775d5ac39d88ac8041adfd1aeba61c
Raj Kathal <rajkathal.skd@gmail.com>
2019-03-21 13:55:44 +0530

    till 11

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -9,7 +9,6 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 export class CreateEmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
-  fullNameLength = 0;
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
@@ -22,40 +21,27 @@ export class CreateEmployeeComponent implements OnInit {
         proficiency: ['beginner']
       })
     });
-
-    this.employeeForm.valueChanges.subscribe((value: any) => {
-      console.log(JSON.stringify(value));
-    });
-    // this.employeeForm.get('fullName').valueChanges.subscribe((value: String) => {
-    //   this.fullNameLength = value.length;
-    // })
   }
 
-  // For Setting all the values
-  /*
-  onLoadDataClick(): void {
-    this.employeeForm.setValue({
-      fullName: 'Pragim Technologies',
-      email: 'raj@gmail.com',
-      skills: {
-        skillName: 'c#',
-        experienceInYears: 5,
-        proficiency: 'beginner'
+  // Recursively looping over all from controls and groups
+  logKeyValuePairs(group: FormGroup): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControls = group.get(key);
+      if (abstractControls instanceof FormGroup) {
+        this.logKeyValuePairs(abstractControls);
+      } else {
+        // console.log('Key:' + key +  ' Value:' + abstractControls.value);
+        // abstractControls.disable();
+        abstractControls.markAsDirty();
+
       }
     });
   }
-  */
 
   // If you want to set only particulr value then you need to use patch value.
 
   onLoadDataClick(): void {
-    this.employeeForm.patchValue({
-      fullName: 'Pragim Technologies',
-      email: 'raj@gmail.com',
-      skills: {
-        skillName: 'c#'
-      }
-    });
+    this.logKeyValuePairs(this.employeeForm);
   }
 
   onSubmit() {
