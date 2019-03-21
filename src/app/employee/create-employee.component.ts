87bb20f973e94e55cf7231e93d99a4fafb701ee0
Raj Kathal <rajkathal.skd@gmail.com>
2019-03-21 23:05:22 +0530

    till 13

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -49,30 +49,22 @@ export class CreateEmployeeComponent implements OnInit {
         proficiency: ['', Validators.required]
       })
     });
+
+    this.employeeForm.valueChanges.subscribe((data) => {
+      this.logValidationErrors(this.employeeForm);
+    });
   }
 
   // Recursively looping over all from controls and groups
-  logValidationErrors(group: FormGroup): void {
-    // Loop through each control key in the FormGroup
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
     Object.keys(group.controls).forEach((key: string) => {
-      // Get the control. The control can be a nested form group
       const abstractControl = group.get(key);
-      // If the control is nested form group, recursively call
-      // this same method
       if (abstractControl instanceof FormGroup) {
         this.logValidationErrors(abstractControl);
-        // If the control is a FormControl
       } else {
-        // Clear the existing validation errors
         this.formErrors[key] = '';
-        if (abstractControl && !abstractControl.valid) {
-          // Get all the validation messages of the form control
-          // that has failed the validation
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
           const messages = this.validationMessages[key];
-          // Find which validation has failed. For example required,
-          // minlength or maxlength. Store that error message in the
-          // formErrors object. The UI will bind to this object to
-          // display the validation errors
           for (const errorKey in abstractControl.errors) {
             if (errorKey) {
               this.formErrors[key] += messages[errorKey] + ' ';
